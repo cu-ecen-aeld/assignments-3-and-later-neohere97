54035ac2c3ec597a3c1fdcfd9e28b46cd26d6c5f
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-10-16 20:29:52 -0600

    added comments

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -34,37 +34,43 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     int size = 0;
     int i, j;
 
+    // When buffer is empty send null
     if (!buffer->full && (buffer->in_offs == buffer->out_offs))
         return NULL;
 
+    // Making a copy of beginning of buffer index
     int buffer_index = buffer->out_offs;
-
     int num_elements;
 
+    // Calculate number of elements present in the buffeer
     if (buffer->full)
         num_elements = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
     else
         num_elements = buffer->in_offs - buffer->out_offs;
 
+    // Iterate over elements
     for (i = 0; i < num_elements; i++)
     {
-
+        // Iterate over characters
         for (j = 0; j < strlen((buffer->entry[buffer_index]).buffptr); j++)
         {
             if (size == char_offset)
             {
+                // When offset is found return the pointer
                 *entry_offset_byte_rtn = j;
                 return &(buffer->entry[buffer_index]);
             }
             else
+                // Update size byte by byte till char offset is reached
+                // TODO: Update logic since iterating over byte by byte is inefficient
                 size++;
         }
         buffer_index++;
-
+        // rollback buffer index when it reaches the end
         if (buffer_index == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
             buffer_index = 0;
     }
-
+    // When offset is not found send null
     return NULL;
 }
 
@@ -78,9 +84,11 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
 
+    // Adds the entry at in_offs position
     (buffer->entry[buffer->in_offs]).buffptr = add_entry->buffptr;
     (buffer->entry[buffer->in_offs]).size = add_entry->size;
 
+    // Updates in_offs and checks if it needs to rollback
     buffer->in_offs += 1;
 
     if (buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
@@ -88,12 +96,14 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 
     if (buffer->full)
     {
+        // Update out_off and check if needs to rollback
         buffer->out_offs += 1;
 
         if (buffer->out_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
             buffer->out_offs = 0;
     }
 
+    // Mark the buffer as full when all elements are added
     if (buffer->in_offs == buffer->out_offs)
     {
         buffer->full = true;
